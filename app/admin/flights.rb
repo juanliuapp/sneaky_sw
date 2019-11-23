e40001206bc390cfcab242c481ae5853984e0dc7
Raghu Betina <raghu@firstdraft.com>
2019-11-23 19:57:06 +0000

    Whitelist columns in Flight for ActiveAdmin

--- app/admin/flights.rb
+++ app/admin/flights.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Flight do
+
+ permit_params :depart_at, :user_id, :description
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
