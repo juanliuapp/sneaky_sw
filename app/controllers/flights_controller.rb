76d61ae6417ae1a18243be231fe4fa345777295f
Raghu Betina <raghu@firstdraft.com>
2019-11-23 19:57:40 +0000

    Integrate Ransack

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -10,7 +10,8 @@ class FlightsController < ApplicationController
   end
 
   def index
-    @flights = Flight.page(params[:page]).per(10)
+    @q = Flight.ransack(params[:q])
+    @flights = @q.result(:distinct => true).includes(:user).page(params[:page]).per(10)
 
     render("flight_templates/index.html.erb")
   end
